371b327abe2b9307313081976731027318356849
marcin-marszalek <66027189+marszal107@users.noreply.github.com>
2022-04-10 14:44:31 +0200

    Initial commit

---
+++ dobot_api/DobotControl.py
@@ -0,0 +1,237 @@
+import threading
+import DobotDllType as dType
+import matplotlib
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+import array as arr
+import pydobot
+from serial.tools import list_ports
+import math
+import time
+
+CON_STR = {
+    dType.DobotConnect.DobotConnect_NoError:  "DobotConnect_NoError",
+    dType.DobotConnect.DobotConnect_NotFound: "DobotConnect_NotFound",
+    dType.DobotConnect.DobotConnect_Occupied: "DobotConnect_Occupied"}
+
+
+class DobotControl:
+
+    def establish_connection(self, port, baudrate):
+        """
+        Establishes connection with Dobot Magician
+
+        :param port:
+        :param baudrate:
+        :return:
+        api
+        state
+        """
+        # Load Dll and get the CDLL object
+        api = dType.load()
+
+        # Connect Dobot
+        state = dType.ConnectDobot(api, port, baudrate)[0]
+        print("Connect status:", CON_STR[state])
+        return api, state
+
+    def home_setup(self, api):
+        """
+        Returns current pose of end effector after homing
+        :param api:
+        :return:
+        """
+        # Clean Command Queued
+        dType.SetQueuedCmdClear(api)
+
+        # Async Motion Params Setting
+        dType.SetHOMEParams(api, 200, 200, 200, 200, isQueued=1)
+        dType.SetPTPCoordinateParams(api, 200, 200, 200, 200)
+        dType.SetPTPJumpParams(api, 10, 200)
+        dType.SetPTPCommonParams(api, 100, 100)
+        dType.SetCPParams(api, 50, 50, 50, 0, 0)
+
+        # Async Home
+        dType.SetHOMECmd(api, temp=0, isQueued=1)
+
+        moveX = 0;
+        moveY = 0;
+        moveZ = 10;
+        moveFlag = -1
+        time.sleep(20)
+        pos = dType.GetPose(api)
+        print(pos)
+        x = pos[0]
+        y = pos[1]
+        z = pos[2]
+        rHead = pos[3]
+        return x, y, z, rHead
+
+
+    def spiral(self):
+        """
+        Returns trajectory points in the form of spiral
+
+        :return:
+        list x, y
+        """
+        theta = np.radians(np.linspace(50, 360*5, 10000))
+        r = theta**2
+        x_2 = r*np.cos(30*theta)/30
+        y_2 = r*np.sin(30*theta)/30
+
+        plt.figure(figsize=[10, 10])
+        plt.plot(x_2, y_2)
+
+        x_2_list = x_2.tolist()
+        y_2_list = y_2.tolist()
+
+        # xy_2_list = []
+        #
+        # for i in range(len(x_2_list)):
+        #     xy_2_list.append([x_2_list[i],
+        #                     y_2_list[i]])
+        #     i+=i
+
+        x_2_list_offset = []
+        y_2_list_offset = []
+        for i in range(0,len(x_2_list)-1,1):
+            x_2_list_offset.append((x_2_list[i] + 250))
+            y_2_list_offset.append(y_2_list[i])
+            i+=i
+        x_2_list_offset.reverse()
+        y_2_list_offset.reverse()
+        return x_2_list_offset, y_2_list_offset
+
+    def execute_trajectory(self, x_pos, y_pos):
+        """
+        Executes given trajectory via points
+        :param x_pos:
+        :param y_pos:
+        :return:
+        """
+        # Async PTP Motion
+        for i in range(0, len(x_pos), 1):
+            """if i % 2 == 0:
+                offset = 50
+            else:
+                offset = -50"""
+            # lastIndex = dType.SetPTPCmd(api, dType.PTPMode.PTPMOVLXYZMode, x_2_list_popr[i] - x + 180, y_2_list_popr[i], 135 - 212 - 0.005*(i-1), rHead, isQueued = 1)[0]
+            lastIndex = dType.SetCPCmd(api, 1, x_pos[i] - x + 180, y_pos[i], 135 - 212 - 0.002 * (i - 1), rHead, isQueued=1)[0]
+
+        # Start to Execute Command Queue
+        dType.SetQueuedCmdStartExec(api)
+
+        # Wait for Executing Last Command
+        while lastIndex > dType.GetQueuedCmdCurrentIndex(api)[0]:
+            dType.dSleep(100)
+
+        # Stop to Execute Command Queued
+        dType.SetQueuedCmdStopExec(api)
+
+
+if __name__ == '__main__':
+    dobot = DobotControl()
+    api, state = dobot.establish_connection(port="COM3", baudrate=115200)
+
+    if (state == dType.DobotConnect.DobotConnect_NoError):
+        x, y, z, rHead = dobot.home_setup(api)
+        x_2_list_offset, y_2_list_offset = dobot.spiral()
+        dobot.execute_trajectory(x_2_list_offset, y_2_list_offset)
+    dType.DisconnectDobot(api)
+
+
+
+
+
+
+    #
+    # # Load Dll and get the CDLL object
+    # api = dType.load()
+    #
+    # # Connect Dobot
+    # state = dType.ConnectDobot(api, "COM3", 115200)[0]
+    # print("Connect status:", CON_STR[state])
+    #
+    # if (state == dType.DobotConnect.DobotConnect_NoError):
+    #
+    #     # Clean Command Queued
+    #     dType.SetQueuedCmdClear(api)
+    #
+    #     # Async Motion Params Setting
+    #     dType.SetHOMEParams(api, 200, 200, 200, 200, isQueued=1)
+    #     dType.SetPTPCoordinateParams(api, 200, 200, 200, 200)
+    #     dType.SetPTPJumpParams(api, 10, 200)
+    #     dType.SetPTPCommonParams(api, 100, 100)
+    #     dType.SetCPParams(api, 50, 50, 50, 0, 0)
+    #
+    #     # Async Home
+    #     dType.SetHOMECmd(api, temp=0, isQueued=1)
+    #     # dType.SetPTPCmd(api, dType.PTPMode.PTPMOVLXYZMode, 200, 200, 200, 200)
+    #     moveX = 0;
+    #     moveY = 0;
+    #     moveZ = 10;
+    #     moveFlag = -1
+    #     time.sleep(20)
+    #     pos = dType.GetPose(api)
+    #     print(pos)
+    #     x = pos[0]
+    #     y = pos[1]
+    #     z = pos[2]
+    #     rHead = pos[3]
+    #     # dType.SetPTPCmd(api, dType.PTPMode.PTPMOVLXYZMode, x + 70, 0, z - 40, rHead, isQueued = 1)
+    #     # pos = dType.GetPose(api)
+    #     # x = pos[0]
+    #     # y = pos[1]
+    #     # z = pos[2]
+    #     # rHead = pos[3]
+    #
+    #     theta = np.radians(np.linspace(50, 360 * 5, 5000))
+    #     r = theta ** 2
+    #     x_2 = r * np.cos(30 * theta) / 30
+    #     y_2 = r * np.sin(30 * theta) / 30
+    #
+    #     plt.figure(figsize=[10, 10])
+    #     plt.plot(x_2, y_2)
+    #
+    #     x_2_list = x_2.tolist()
+    #     y_2_list = y_2.tolist()
+    #
+    #     xy_2_list = []
+    #
+    #     for i in range(len(x_2_list)):
+    #         xy_2_list.append([x_2_list[i],
+    #                           y_2_list[i]])
+    #         i += i
+    #
+    #     x_2_list_popr = []
+    #     y_2_list_popr = []
+    #     for i in range(0, len(x_2_list) - 1, 1):
+    #         x_2_list_popr.append((x_2_list[i] + 250))
+    #         y_2_list_popr.append((y_2_list[i]))
+    #         i += i
+    #     x_2_list_popr.reverse()
+    #     y_2_list_popr.reverse()
+    #
+    #     # Async PTP Motion
+    #     for i in range(0, len(x_2_list_popr), 1):
+    #         """if i % 2 == 0:
+    #             offset = 50
+    #         else:
+    #             offset = -50"""
+    #         # lastIndex = dType.SetPTPCmd(api, dType.PTPMode.PTPMOVLXYZMode, x_2_list_popr[i] - x + 180, y_2_list_popr[i], 135 - 212 - 0.005*(i-1), rHead, isQueued = 1)[0]
+    #         lastIndex = dType.SetCPCmd(api, 1, x_2_list_popr[i] - x + 180, y_2_list_popr[i], 135 - 212 - 0.002 * (i - 1), rHead, isQueued=1)[0]
+    #
+    #     # Start to Execute Command Queue
+    #     dType.SetQueuedCmdStartExec(api)
+    #
+    #     # Wait for Executing Last Command
+    #     while lastIndex > dType.GetQueuedCmdCurrentIndex(api)[0]:
+    #         dType.dSleep(100)
+    #
+    #     # Stop to Execute Command Queued
+    #     dType.SetQueuedCmdStopExec(api)
+    #
+    # # Disconnect Dobot
+    # dType.DisconnectDobot(api)
---
+++ dobot_api/spiral.py
@@ -0,0 +1,89 @@
+import matplotlib
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+import array as arr
+import pydobot
+from serial.tools import list_ports
+import math
+#import cv2 
+
+
+def spirala():
+    theta = np.radians(np.linspace(50, 360*5, 5000))
+    r = theta**2
+    x_2 = r*np.cos(30*theta)/30
+    y_2 = r*np.sin(30*theta)/30
+
+    plt.figure(figsize=[10, 10])
+    plt.plot(x_2, y_2)
+
+    x_2_list = x_2.tolist()
+    y_2_list = y_2.tolist()
+
+    xy_2_list = []
+
+    for i in range(len(x_2_list)):
+        xy_2_list.append([x_2_list[i], 
+                        y_2_list[i]])
+        i+=i
+
+    x_2_list_popr = []
+    y_2_list_popr = []
+    for i in range(0,len(x_2_list)-1,1):
+        x_2_list_popr.append((x_2_list[i] + 250)) 
+        y_2_list_popr.append((y_2_list[i] + 250)) 
+        i+=i
+    x_2_list_popr.reverse()
+    y_2_list_popr.reverse()
+    print(x_2_list_popr[1] + 270 -200)
+    return xy_2_list
+
+
+def connect_to_robot():
+    available_ports = list_ports.comports()
+    print(f'available ports: {[x.device for x in available_ports]}')
+    port = available_ports[0].device
+
+    device = pydobot.Dobot(port=port, verbose=True)
+
+    robot_responses = []
+    xy_2_list = spirala()
+
+    for i in range(len(xy_2_list)):
+        z = 0
+        r = 0
+        xy_cords = xy_2_list[i]
+        device.move_to(xy_cords[0], xy_cords[1], z, r, wait=True)
+        (x, y, z, r, j1, j2, j3, j4) = device.pose()
+        print(f'x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}')
+        robot_responses.append(x, y)
+        i+=i
+
+    device.close()
+spirala()
+#connect_to_robot()
+"""
+errors_list = []
+
+for i in range(len(robot_responses)):
+    xy_cords = xy_2_list[i]
+    robot_response = robot_responses[i]
+    errors_list.append([((xy_cords[0] - robot_response[0]) / xy_cords[0]), 
+                        ((xy_cords[1] - robot_response[1]) / xy_cords[1])])
+    i+=i
+
+x_robot_responses = []
+y_robot_responses = []
+
+for i in range(len(robot_responses)):
+    robot_response = robot_responses[i]
+    x_robot_responses.append(robot_response[0])
+    y_robot_responses.append(robot_response[1])
+    i+=i
+
+plt.figure(figsize=[10, 10])
+plt.plot(x_robot_responses, y_robot_responses)
+"""
+plt.show()
+
